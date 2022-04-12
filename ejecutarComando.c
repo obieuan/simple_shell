943741978e144c8df8f1922ba5918b3a328b5737
obieuan <obieuan@gmail.com>
2022-04-11 20:12:07 -0500

    update

--- ejecutarComando.c
+++ ejecutarComando.c
@@ -1,29 +1,33 @@
 #include "shell.h"
-/* funcion que ejeuta el comando */
+/*
+* Función que ejecuta el comando que se le pasa como argumento.
+ * @args: Arreglo de char que contiene los argumentos del comando.
+ * Return: nothing.
+ */
 void ejecutarComando(char **args)
 {
-    pid_t pid;
-    int status;
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
-    }
-    else if (pid < 0)
-    {
-        perror("Error");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        do
-        {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else if (pid < 0)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
