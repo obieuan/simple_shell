08254ee7d838bafad48727c9e51129401777a12d
obieuan <obieuan@gmail.com>
2022-04-11 20:41:00 -0500

    update

--- imprimirArgumentos.c
+++ imprimirArgumentos.c
@@ -2,9 +2,10 @@
 /* funcion que imprime los argumentos que se le pasan */
 void imprimirArgumentos(char **args)
 {
-    int i;
-    for (i = 0; args[i] != NULL; i++)
-    {
-        printf("%i: %s\n", i, args[i]);
-    }
+	int i;
+
+	for (i = 0; args[i] != NULL; i++)
+	{
+		printf("%i: %s\n", i, args[i]);
+	}
 }
